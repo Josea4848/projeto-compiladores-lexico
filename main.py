74a5d66b7c651fb0044c141f2964644ce42c822d
José Alves <josealves3416@gmail.com>
2023-12-15 00:01:55 -0300

    começo ao autômato

--- main.py
+++ main.py
@@ -2,15 +2,55 @@ import re
 
 class finiteAutomation:
   def __init__(self):
-    word = ""
-  
+    self.estado = 1
+    self.alphaNum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    self.tabela = list()
+
   def transition(self, char):
-    key_words = ("program", "var", "integer", "real", "boolean", "procedure", "begin",
-    "end", "if", "then", "else", "while", "do", "not")
-    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    numbers = "0123456789"
-    
+    match self.estado:
+      case 1:
+        if char == "p":
+          self.estado = 2
+      case 2:
+        if char == "r":
+          self.estado = 3
+      case 3:
+        if char == "o":
+          self.estado = 4
+      case 4: 
+        if char == "g":
+          self.estado = 5
+      case 5:
+        if char == "r":
+          self.estado = 6
+      case 6: 
+        if char == "a":
+          self.estado = 7
+      case 7: 
+        if char == "m":
+          self.estado = 8
+      case 8: 
+        print(f"{char} não está em alpha: {char not in self.alphaNum}")
+        if char not in self.alphaNum:
+          self.tabela.append("program")
+          self.estado = 1
+        else:
+          self.estado = 38
 
   def programInput(self, program):
     for char in program:
       self.transition(char)
+
+
+#Lendo programa de um .txt
+programFile = open("main.txt", "r")
+
+#Criando string única com todo o programa
+linhas = programFile.readlines()
+linhas = [linha.replace("\n", " ") for linha in linhas]
+linhaStr = "".join(linhas)
+
+#Instanciando um objeto
+lexical = finiteAutomation()
+lexical.programInput(linhaStr)
+print(lexical.tabela) 

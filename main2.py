91801cbe20659205cc40a265c2ec898563b91e4e
José Alves <josealves3416@gmail.com>
2023-12-17 16:21:07 -0300

    finalização do código essencial

--- main2.py
+++ main2.py
@@ -1,33 +1,51 @@
 import re
 
-patterns = [
-    ('REAL', r'\b\d+\.\d+\b'), #REAIS
-    ('INTEGER', r'\b\d+\b'),  #KEYWORD: INTEIROS
-    ('KEYWORD',r'\b(var|program|real|integer|var|boolean|procedure|begin|end|if|then|else|while|do|not)\b'), #KEYWORD: VAR
-    ('multiplicative_operator', r'\b(\*|/|and)\b'),
-    ('ADDITIVE_OPERATORS', r'\b(\+|-|or)\b'),  # OPERADORES ADITIVOS
-    ('IDENTIFICADOR', r'\b[a-zA-z|_]\w*\b'),
-    ('WHITESPACE', r'\s+'), # Identificacao do espaco em branco
-    ('relational_operator', r'<(=|>)?|>=?|=')
-]
+class Lexical:
+    def __init__(self):
+      self.patterns = [
+      ('real', r'\b\d+\.\d+\b'), #REAIS
+      ('integer', r'\b\d+\b'),  #KEYWORD: INTEIROS
+      ('keyword',r'\b(var|program|real|integer|var|boolean|procedure|begin|end|if|then|else|while|do|not)\b'), #KEYWORD: VAR 
+      ('multiplicative_operator', r'\b(\*|/|and)\b'),
+      ('additive_operators', r'\b(\+|-|or)\b'),  # OPERADORES ADITIVOS
+      ('identifier', r'\b[a-zA-z|_]\w*\b'),
+      ('WHITESPACE', r'\s+'), # Identificacao do espaco em branco
+      ('assignment',r':='),
+      ('relational_operator', r'<(=|>)?|>=?|='),
+      ('delimiter',r'(;|\.|\(|\)|,|:)'),
+      ('comment',r'{(.|\n)*}')]
+      self.token_regex = '|'.join('(?P<%s>%s)' % pair for pair in self.patterns)
+      self.lexer = re.compile(self.token_regex)
+      self.table = {"Token":list(),"Classificação":list(),"Line":list()}
+
+    def tokenizador(self, program, line):
+      for match in self.lexer.finditer(program):
+          for name, pattern in self.patterns:
+              token = match.group(name)
+              if token and name != 'WHITESPACE':
+                  self.addTable(token, name, line)
+                  break
+
+    def addTable(self, token, classificao, line):
+       self.table["Token"].append(token)
+       self.table["Classificação"].append(classificao)
+       self.table["Line"].append(line)
+
+    def showTable(self):
+      arquivo = open("tabela2.csv", "w+")
+      arquivo.write("Token;Classificação;Linha\n")
+
+      for index in range(0, len(self.table["Token"])):
+        arquivo.write(f""""{self.table['Token'][index]}";{self.table['Classificação'][index]}; {self.table['Line'][index]}\n""")
 
 # Input program
 arquivo = open("main.txt", "r")
 program = arquivo.readlines()
 
-# Compilacao dos padores regex
-token_regex = '|'.join('(?P<%s>%s)' % pair for pair in patterns)
-lexer = re.compile(token_regex)
+#Instanciando um objeto
+analisador = Lexical()
 
-# Tokenizacao (separacao dos tokens) do string de entrada
-tokens = []
-for line in program:
-  for match in lexer.finditer(line):
-      for name, pattern in patterns:
-          token = match.group(name)
-          if token:
-              tokens.append((name, token))
-              break
+for index in range(0, len(program)):
+  analisador.tokenizador(program[index], index+1)
 
-for token in tokens:
-    print(token)
+analisador.showTable()
